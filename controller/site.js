8b101abb3eac7ea80aae52ebf9d42765e6af76f3
xiaoz <24409917@qq.com>
2013-04-17 22:05:19 +0800

    init

--- controller/site.js
+++ controller/site.js
@@ -8,8 +8,53 @@ var userDao = require('../dao/user.js');
  */
 exports.index = function(req, res, next) {
     
-        res.render('index', {
+        res.render('front/index', {
         });
   
+};
+/**
+ * 网站首页主体数据
+ */
+exports.about = function(req, res, next) {
+    
+    res.render('front/about', {
+    });
+
+};
+/**
+ * 网站首页关于我们
+ */
+exports.product = function(req, res, next) {
+    
+    res.render('front/product', {
+    });
+
+};
+/**
+ * 网站首页产品展示
+ */
+exports.news = function(req, res, next) {
+    
+    res.render('front/news', {
+    });
+
+};
+/**
+ * 网站首页客户案例
+ */
+exports.cases = function(req, res, next) {
+    
+    res.render('front/cases', {
+    });
+
+};
+/**
+ * 网站首页联系我们
+ */
+exports.contact = function(req, res, next) {
+    
+    res.render('front/contact', {
+    });
+
 };
 
--- route.js
+++ route.js
@@ -1,8 +1,20 @@
 var site_ctrl = require('./controller/site.js');
 
 
-exports = module.exports = function(app) {// routes
+exports = module.exports = function(app) {
     // 首页
     app.get('/', site_ctrl.index);
+	// 关于我们
+	app.get('/about', site_ctrl.about);
+	// 产品展示
+	app.get('/product', site_ctrl.product);
+	// 新闻 blog
+	app.get('/news', site_ctrl.news);
+	//客户案例
+	app.get('/cases', site_ctrl.cases);
+	// 联系我们
+	app.get('/contact', site_ctrl.contact);
+	
+	
 
 };
