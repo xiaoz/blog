2d64ea681e9d128933984542cb9102e0663077f7
xiaoz <24409917@qq.com>
2013-05-24 09:40:34 +0800

    init

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var download_dir = __dirname + '/home';
 app.configure(function() {
     app.set('view engine', 'html');
     app.set('views', __dirname + '/view');
-    var one_year = 1000 * 60 * 60 * 24 * 30;
-    app.use(express.static(static_dir, {
-        maxAge : one_year
-    }));
-    app.set('view cache', true);
+    //var one_year = 1000 * 60 * 60 * 24 * 30;
+    //app.use(express.static(static_dir, {
+    //    maxAge : one_year
+    //}));
+    //app.set('view cache', true);
     
     app.register('.html', require('ejs'));
     app.use(express.bodyParser({
